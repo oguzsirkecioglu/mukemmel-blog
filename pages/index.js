3ced51e556c93eaff93d38df810bc3199f62c5af
oguz <oguzsirkecioglu10@gmail.com>
2020-02-15 19:54:08 +0300

    guzellesti

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,25 @@ import ReactMarkdown from "react-markdown";
 const Home = ({ posts }) => (
   <div className="container">
     <Head>
-            <title>Home</title>
+            <title>Oğuz Sirkecioğlu | Developer</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <div className="hero">
-      <h1 className="boss">Oğuz Sirkecioğlu</h1>
-<h2 className="intro"> Industrial Engineering Student and Developer </h2>
+      <p className="boss">OĞUZ SİRKECİOĞLU</p>
+      
+<h2 className="intro"> IE STUDENT / DEVELOPER </h2>
+<hr className="cizgi"></hr>
       <div className="hero-social-links">
 
       
-        <Link href="https://www.twitter.com/oloading1">
-          <a className="social-link">Twitter</a>
-        </Link>
-        <Link href="https://tr.linkedin.com/in/oğuz-sirkecioğlu-37a200198">
-          <a className="social-link">LinkedIn</a>
-        </Link>
-        <Link href="https://www.instagram.com/oguzsirkecioglu">
-          <a className="social-link">Instagram</a>
-        </Link>
+      <a className="social-link" href="https://www.linkedin.com/oguzsirkecioglu">  <input type="image" className="twitter" src="https://img.icons8.com/plasticine/2x/linkedin.png" /></a>
+
+        <a className="social-link" href="https://www.instagram.com/oguzsirkecioglu">  <input type="image" className="insta" src="https://cdn2.iconfinder.com/data/icons/instagram-classic/512/instagram-square-3d-512.png" /></a>
+
+        <a className="social-link" href="https://www.twitter.com/oloading1">  <input type="image" className="twitterr" src="https://img.icons8.com/plasticine/2x/twitter.png" /></a>
+
+      
       </div>
     </div>
 
@@ -33,38 +33,72 @@ const Home = ({ posts }) => (
 
     <style jsx>{`
       .container {
-        max-width: 650px;
-        width: 100%;
-        margin: 1 auto;
-        padding-top: 120px;
+        min-width: 800px;
+        min-height: 700px;
+        padding-top:110px;
+        margin: 9px auto;
+        color:white;
+        background:black;
+        margin: -1% ; 
+        
+        
       }
-      
+      .cizgi{
+        border: 0.4pt solid grey;      
+        width: 9%;
+        margin-top:40px;
+        
+      }
       .intro{
         font-family: arial;
-        font-size:15px;
+        font-size:13px;
         color: grey;
-        padding-top:12px;
+        padding-top: px;
+        font-family: Lucida Grande;
+      
+        text-a
       }
 
       .hero {
         text-align: center;
         margin: 96px 0;
+        max-height: 950px;
         
       }
 
       .social-link {
         margin-right: 8px;
+        color:grey;
+       
 
       }
-      
+      .twitter{
+        padding-top:1px;
+height:62px;
+      }
+.twitterr{
+padding:px;
+height:62px;
+}
+.insta{
+  padding-top: 17px;
+ padding:9px; 
+height:42px;
+}
+
+      }
       .hero-social-links{
         padding-top:34px;
+        
       }
 
       .boss {
-        font-size: 48px;
-        font-family: "Times New Roman", Times, serif;
+        font-size: 26px;
+        font-family: Lucida Grande;
         padding-top:1px;
+        letter-spacing: 3.5px;
+        
+        
 
         }
 
